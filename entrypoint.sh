fc33d66def3c91bb82565c6e429ff44e70533f99
shigaevaler@gmail.com <shigaevaler@gmail.com>
2023-03-14 21:40:11 +0400

    entrypoint static fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,5 @@ cd webgis/
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata fixtures/mems_dump.json
-#python manage.py loaddata fixtures/images.json
-#python manage.py initadmin
-#python manage.py test
-##python manage.py bot &
-#
-##celery -A app worker -l INFO &
+python manage.py collectstatic
 gunicorn --bind 0.0.0.0:8000 webgis.wsgi:application
-#python3 manage.py runserver
