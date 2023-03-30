561296ef114266d2711b77d7ce1f2b95a349ddbd
shigaevaler@gmail.com <shigaevaler@gmail.com>
2023-03-30 21:34:47 +0400

    editor feature

--- webgis/static/scripts/app_map.js
+++ webgis/static/scripts/app_map.js
@@ -34,8 +34,21 @@ var vm = new Vue({
       moveState: false,
       deleteState: false,
       helpText: "",
-      createForm: false,
 
+      createForm: false,
+      clickLatLng: "",
+      pointSelected: false,
+      form: {
+        name: "",
+        district: "",
+        type: "",
+        av_order: "",
+        notes: "",
+        X: "",
+        Y: ""
+      },
+      errors: [],
+      serviceError: ""
     }
   },
   methods: {
@@ -48,7 +61,7 @@ var vm = new Vue({
     onEachFeatureFunction(feature, layer) {
       layer.setIcon(this.icon);
       layer.bindTooltip(
-        "<div>" +
+        "<div class='font'>" +
           feature.properties.name +
           "</div>",
         { permanent: false, sticky: true }
@@ -65,7 +78,83 @@ var vm = new Vue({
     },
     clickHandler(event){
       if(this.createState){
+        if (!this.pointSelected){
+          this.clickLatLng = event.latlng
+        }
         this.createForm = true;
+        this.pointSelected = true;
+      }
+    },
+    async submit() {
+      if(this.validate()){
+        this.loading = true;
+        this.errors = [];
+        result = await this.createNewObject();
+        if(result.status == 201){
+          await this.reloadObjects();
+        }else{
+          this.serviceError = "Service is temporarily unavailable"
+        }
+        this.loading = false;
+      }
+      
+    },
+    validate(){
+      if(this.clickLatLng){
+        this.form.X = this.clickLatLng.lat;
+        this.form.Y = this.clickLatLng.lng;
+      }
+      console.log()
+      this.errors = [];
+      if(!this.form.name){
+        this.errors.push("Enter a name")
+      }
+      if(!this.form.type){
+        this.errors.push("Select a type")
+      }
+      if(!this.form.X || !this.form.Y){
+        this.errors = [];
+        this.errors.push("This service is temporarily unavailable")
+      }
+      return !this.errors.length
+
+    },
+    async createNewObject(){
+      this.createForm = false;
+      const params ={
+        "name": this.form.name,
+        "region": 16,
+        "district": this.form.district,
+        "type": this.form.type,
+        "av_order": this.form.av_order,
+        "status": "regional",
+        "pos": 0,
+        "notes": this.form.notes,
+        "x": this.form.X,
+        "y": this.form.Y
+      }
+      const options = {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(params)
+      }
+      data = await fetch('/api/objects/create', options);
+      return data;
+    },
+    async reloadObjects(){
+      const response = await fetch("/api/objects/");
+      if(response.status == 200){
+        const data = await response.json();
+        localStorage.setItem("points", JSON.stringify(data));
+        this.geojson = JSON.parse(localStorage.getItem("points"));
+        this.types = this.getTypes(this.geojson);
+        return true;
+      }else{
+        this.serviceError = "This application is temporarily unavailable"
+        return false;
       }
     }
   },
@@ -103,6 +192,7 @@ var vm = new Vue({
         this.moveState = false
         this.deleteState = false
         this.helpText = "Click on the map"
+        console.log(this.$refs.map)
       }
     },
     moveState(){
@@ -121,14 +211,13 @@ var vm = new Vue({
       }
       
     },
+    
   },
   async created() {
     this.loading = true;
-    const response = await fetch("/api/objects/");
-    const data = await response.json();
-    localStorage.setItem("points", JSON.stringify(data));
-    this.geojson = JSON.parse(localStorage.getItem("points"));
-    this.types = this.getTypes(this.geojson);
-    this.loading = false;
+    result = await this.reloadObjects();
+    if(result){
+      this.loading = false;
+    }
   }
 })
